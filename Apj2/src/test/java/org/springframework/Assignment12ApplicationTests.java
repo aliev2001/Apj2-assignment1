e2fe2358928c1ae55bf4a722eac3af1874e469db
Zangar <ali.zangar00@mail.ru>
2021-01-04 21:41:23 +0600

    commit

---
+++ Apj2/src/test/java/org/springframework/Assignment12ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
